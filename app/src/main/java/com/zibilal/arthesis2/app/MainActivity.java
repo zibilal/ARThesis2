768f2803aa8606ffb39b42f3055a380275e620ac
Bilal Muhammad <bmuhamm@gmail.com>
2015-03-09 01:44:43 +0700

    New gradle sets

--- app/src/main/java/com/zibilal/arthesis2/app/MainActivity.java
+++ app/src/main/java/com/zibilal/arthesis2/app/MainActivity.java
@@ -29,14 +29,6 @@ public class MainActivity extends Activity {
         init();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
     private void init(){
         objects = new ArrayList<ActivityObjects>();
         objects.add(new ActivityObjects("ARActivity", ARActivity.class));
@@ -56,17 +48,6 @@ public class MainActivity extends Activity {
             }
         });
 
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     class ActivityObjects {
